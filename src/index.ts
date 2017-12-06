bab97547c006c6dc9ad78bf387a45a068599cbf6
Jacques Kvam <jwkvam@gmail.com>
2017-12-05 17:42:15 -0800

    shift escape to leave vim command mode

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as CodeMirror
+  from 'codemirror';
+
 import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
@@ -9,6 +12,10 @@ import {
 import {
   CodeMirrorEditor
 } from '@jupyterlab/codemirror'
+
+// import {
+//   CommandRegistry
+// } from '@phosphor/commands';
 
 import '../style/index.css';
 
@@ -25,8 +32,9 @@ const extension: JupyterLabPlugin<void> = {
 
 class VimCell {
 
-    constructor(tracker: INotebookTracker) {
+    constructor(app: JupyterLab, tracker: INotebookTracker) {
         this._tracker = tracker;
+        this._app = app;
         this._tracker.activeCellChanged.connect(this._onActiveCellChanged, this);
     }
 
@@ -34,21 +42,78 @@ class VimCell {
         // if (this._prevActive && !this._prevActive.isDisposed) {
         //     this._prevActive.metadata.changed.disconnect(this._onMetadataChanged, this);
         // }
+        // let commands = new CommandRegistry();
+        //
+        // console.log(commands);
+        // debugger;
         let activeCell = this._tracker.activeCell;
-        console.log(activeCell);
-        console.log('activating vimmers!!!');
-        let editor = activeCell.editor as CodeMirrorEditor;
-        editor.setOption('keyMap', 'vim');
-        console.log(editor);
+        if (activeCell !== null) {
+            console.log(activeCell);
+            console.log('activating vimmers!!!');
+            let editor = activeCell.editor as CodeMirrorEditor;
+            editor.setOption('keyMap', 'vim');
+            let extraKeys = editor.getOption('extraKeys') || {};
+
+            extraKeys['Esc'] = CodeMirror.prototype.leaveInsertMode;
+            extraKeys['Ctrl-j'] = CodeMirror.prototype.leaveInsertMode;
+            extraKeys['Ctrl-J'] = CodeMirror.prototype.leaveInsertMode;
+            // extraKeys['Shift-Esc'] = CodeMirror.prototype.leaveNormalMode;
+            extraKeys['Ctrl-C'] = CodeMirror.prototype.leaveInsertMode;
+
+            editor.setOption('extraKeys', extraKeys);
+            // const {commands, shell} = this._app;
+            // console.log(commands);
+            // console.log(shell);
+            // debugger;
+            
+            console.log(editor);
+        }
     }
 
     private _tracker: INotebookTracker;
+    private _app: JupyterLab;
 }
 
 function activateCellVim(app: JupyterLab, tracker: INotebookTracker): Promise<void> {
     console.log('activating vim!');
-    const vimcell = new VimCell(tracker);
-    console.log(vimcell);
+
+    Promise.all([app.restored]).then(([args]) => {
+        console.log('app started');
+        const {commands} = app;
+        console.log('number of commands');
+        console.log(commands.keyBindings.length);
+        console.log(commands);
+
+        // let idx = commands.keyBindings.findIndex(function (el) {
+        //     return (el.keys[0] == "Escape" && el.selector == ".jp-Notebook.jp-mod-editMode")
+        // })
+        // commands.keyBindings.splice(idx, 1)
+        
+        // debugger;
+        commands.addKeyBinding({
+            selector: '.jp-Notebook.jp-mod-editMode',
+            keys: ['Escape'],
+            command: 'notebook:enter-edit-mode'
+        });
+        commands.addKeyBinding({
+            selector: '.jp-Notebook.jp-mod-editMode',
+            keys: ['Shift Escape'],
+            command: 'notebook:enter-command-mode'
+        });
+
+        const vimcell = new VimCell(app, tracker);
+        console.log(vimcell);
+        // commands.addCommand('vim-command-mode', {
+        //     label: 'Edit Mode',
+        //     // execute: () => { nbWidget.notebook.mode = 'edit'; }
+        //     execute: () => { CodeMirror.prototype.leaveInsertMode; }
+        // });
+        // commands.addKeyBinding({
+        //     selector: '.jp-Notebook.jp-mod-editMode',
+        //     keys: ['Escape'],
+        //     command: 'vim-command-mode'
+        // })
+    });
 
   return Promise.resolve();
 }
