f2dc0f80f1b93dce327390176244c065bee4cb46
Jacques Kvam <jwkvam@gmail.com>
2017-12-07 22:54:37 -0800

    "j" and "k" move to next/prev cell if on last line of cell

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import * as CodeMirror
-  from 'codemirror';
+import * as CodeMirror from 'codemirror';
 
 import {
-  JupyterLab, JupyterLabPlugin
+    JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
 import {
-  INotebookTracker, NotebookActions, NotebookPanel
+    INotebookTracker, NotebookActions, NotebookPanel
 } from '@jupyterlab/notebook'
 
 import {
-  CodeMirrorEditor
+    MarkdownCell
+} from '@jupyterlab/cells'
+
+import {
+    CodeMirrorEditor
 } from '@jupyterlab/codemirror'
 
 import {
-  ReadonlyJSONObject
+    ReadonlyJSONObject
 } from '@phosphor/coreutils';
 
 // import {
@@ -46,14 +49,8 @@ class VimCell {
         // if (this._prevActive && !this._prevActive.isDisposed) {
         //     this._prevActive.metadata.changed.disconnect(this._onMetadataChanged, this);
         // }
-        // let commands = new CommandRegistry();
-        //
-        // console.log(commands);
-        // debugger;
         let activeCell = this._tracker.activeCell;
         if (activeCell !== null) {
-            // console.log(activeCell);
-            // console.log('activating vimmers!!!');
             let editor = activeCell.editor as CodeMirrorEditor;
             editor.setOption('keyMap', 'vim');
             let extraKeys = editor.getOption('extraKeys') || {};
@@ -63,12 +60,97 @@ class VimCell {
             extraKeys['Ctrl-C'] = CodeMirror.prototype.leaveInsertMode;
 
             editor.setOption('extraKeys', extraKeys);
-            // const {commands, shell} = this._app;
-            // console.log(commands);
-            // console.log(shell);
-            // debugger;
+            const {commands} = this._app;
             
-            // console.log(editor);
+            let lcm = CodeMirror as any;
+            let lvim = lcm.Vim as any;
+            lvim.defineMotion('moveByLinesOrCell', (cm: any, head: any, motionArgs: any, vim: any) => {
+                let cur = head;
+                let endCh = cur.ch;
+                let current_cell = activeCell;
+                // TODO: these references will be undefined
+                // Depending what our last motion was, we may want to do different
+                // things. If our last motion was moving vertically, we want to
+                // preserve the HPos from our last horizontal move.  If our last motion
+                // was going to the end of a line, moving vertically we should go to
+                // the end of the line, etc.
+                switch (vim.lastMotion) {
+                    case 'moveByLines':
+                    case 'moveByDisplayLines':
+                    case 'moveByScroll':
+                    case 'moveToColumn':
+                    case 'moveToEol':
+                        // JUPYTER PATCH: add our custom method to the motion cases
+                    case 'moveByLinesOrCell':
+                        endCh = vim.lastHPos;
+                        break;
+                    default:
+                        vim.lastHPos = endCh;
+                }
+                let repeat = motionArgs.repeat + (motionArgs.repeatOffset || 0);
+                let line = motionArgs.forward ? cur.line + repeat : cur.line - repeat;
+                let first = cm.firstLine();
+                let last = cm.lastLine();
+                // Vim cancels linewise motions that start on an edge and move beyond
+                // that edge. It does not cancel motions that do not start on an edge.
+
+                // JUPYTER PATCH BEGIN
+                // here we insert the jumps to the next cells
+                if(line < first || line > last){
+                    // var current_cell = ns.notebook.get_selected_cell();
+                    // var current_cell = tracker.activeCell;
+                    var key = '';
+                    if (current_cell.model.type == 'markdown') {
+                        (current_cell as MarkdownCell).rendered = true;
+                        // current_cell.execute();
+                    }
+                    if (motionArgs.forward) {
+                        // ns.notebook.select_next();
+                        commands.execute('notebook:move-cursor-down')
+                        key = 'j';
+                    } else {
+                        // ns.notebook.select_prev();
+                        commands.execute('notebook:move-cursor-up')
+                        key = 'k';
+                    }
+                    // ns.notebook.edit_mode();
+                    // var new_cell = ns.notebook.get_selected_cell();
+                    // if (current_cell !== new_cell && !!new_cell) {
+                    //     // The selected cell has moved. Move the cursor at very end
+                    //     var cm2 = new_cell.code_mirror;
+                    //     cm2.setCursor({
+                    //         ch:   cm2.getCursor().ch,
+                    //         line: motionArgs.forward ? cm2.firstLine() : cm2.lastLine()
+                    //     });
+                    //     // Perform remaining repeats
+                    //     repeat = motionArgs.forward ? line - last : first - line;
+                    //     repeat -= 1;
+                    //     if (Math.abs(repeat) > 0) {
+                    //         CodeMirror.Vim.handleKey(cm2, repeat + key);  // e.g. 4j, 6k, etc.
+                    //     }
+                    // }
+                    return;
+                }
+                // JUPYTER PATCH END
+
+                // if (motionArgs.toFirstChar){
+                //     endCh = findFirstNonWhiteSpaceCharacter(cm.getLine(line));
+                //     vim.lastHPos = endCh;
+                // }
+                vim.lastHSPos = cm.charCoords(CodeMirror.Pos(line, endCh), 'div').left;
+                return (CodeMirror as any).Pos(line, endCh);
+            });
+
+            lvim.mapCommand(
+                'k', 'motion', 'moveByLinesOrCell',
+                { forward: false, linewise: true },
+                { context: "normal" }
+            )
+            lvim.mapCommand(
+                'j', 'motion', 'moveByLinesOrCell',
+                { forward: true, linewise: true },
+                { context: "normal" }
+            )
         }
     }
 
@@ -77,92 +159,6 @@ class VimCell {
 }
 
 function activateCellVim(app: JupyterLab, tracker: INotebookTracker): Promise<void> {
-    // console.log('activating vim!');
-    
-    // var moveByLinesOrCell = function moveByLinesOrCell(cm, head, motionArgs, vim) {
-    //     var cur = head;
-    //     var endCh = cur.ch;
-    //     // TODO: these references will be undefined
-    //     // Depending what our last motion was, we may want to do different
-    //     // things. If our last motion was moving vertically, we want to
-    //     // preserve the HPos from our last horizontal move.  If our last motion
-    //     // was going to the end of a line, moving vertically we should go to
-    //     // the end of the line, etc.
-    //     switch (vim.lastMotion) {
-    //         case this.moveByLines:
-    //         case this.moveByDisplayLines:
-    //         case this.moveByScroll:
-    //         case this.moveToColumn:
-    //         case this.moveToEol:
-    //             // JUPYTER PATCH: add our custom method to the motion cases
-    //         case moveByLinesOrCell:
-    //             endCh = vim.lastHPos;
-    //             break;
-    //         default:
-    //             vim.lastHPos = endCh;
-    //     }
-    //     var repeat = motionArgs.repeat + (motionArgs.repeatOffset || 0);
-    //     var line = motionArgs.forward ? cur.line + repeat : cur.line - repeat;
-    //     var first = cm.firstLine();
-    //     var last = cm.lastLine();
-    //     // Vim cancels linewise motions that start on an edge and move beyond
-    //     // that edge. It does not cancel motions that do not start on an edge.
-    //
-    //     // JUPYTER PATCH BEGIN
-    //     // here we insert the jumps to the next cells
-    //     if(line < first || line > last){
-    //         var current_cell = ns.notebook.get_selected_cell();
-    //         var key = '';
-    //         if (current_cell.cell_type == 'markdown') {
-    //             current_cell.execute();
-    //         }
-    //         if (motionArgs.forward) {
-    //             ns.notebook.select_next();
-    //             key = 'j';
-    //         } else {
-    //             ns.notebook.select_prev();
-    //             key = 'k';
-    //         }
-    //         ns.notebook.edit_mode();
-    //         var new_cell = ns.notebook.get_selected_cell();
-    //         if (current_cell !== new_cell && !!new_cell) {
-    //             // The selected cell has moved. Move the cursor at very end
-    //             var cm2 = new_cell.code_mirror;
-    //             cm2.setCursor({
-    //                 ch:   cm2.getCursor().ch,
-    //                 line: motionArgs.forward ? cm2.firstLine() : cm2.lastLine()
-    //             });
-    //             // Perform remaining repeats
-    //             repeat = motionArgs.forward ? line - last : first - line;
-    //             repeat -= 1;
-    //             if (Math.abs(repeat) > 0) {
-    //                 CodeMirror.Vim.handleKey(cm2, repeat + key);  // e.g. 4j, 6k, etc.
-    //             }
-    //         }
-    //         return;
-    //     }
-    //     // JUPYTER PATCH END
-    //
-    //     if (motionArgs.toFirstChar){
-    //         endCh = findFirstNonWhiteSpaceCharacter(cm.getLine(line));
-    //         vim.lastHPos = endCh;
-    //     }
-    //     vim.lastHSPos = cm.charCoords(CodeMirror.Pos(line, endCh), 'div').left;
-    //     return CodeMirror.Pos(line, endCh);
-    // };
-    //
-    // CodeMirror.Vim.defineMotion('moveByLinesOrCell', moveByLinesOrCell);
-    // CodeMirror.Vim.mapCommand(
-    //     "<Plug>(vim-binding-k)", "motion", "moveByLinesOrCell",
-    //     {forward: false, linewise: true },
-    //     {context: "normal"}
-    // )
-    // CodeMirror.Vim.mapCommand(
-    //     "<Plug>(vim-binding-j)", "motion", "moveByLinesOrCell",
-    //     {forward: true, linewise: true },
-    //     {context: "normal"}
-    // )
-
 
     Promise.all([app.restored]).then(([args]) => {
         // console.log('app started');
@@ -189,6 +185,16 @@ function activateCellVim(app: JupyterLab, tracker: INotebookTracker): Promise<vo
         //     return (el.keys[0] == "Escape" && el.selector == ".jp-Notebook.jp-mod-editMode")
         // })
         // commands._keyBindings.splice(idx, 1)
+        
+        // (CodeMirror as any).Vim.defineMotion('moveByLinesOrCell', (cm: any, head: any, motionArgs: any, vim: any) => {
+
+        // ((CodeMirror as any).Vim as any).defineMotion('moveByLinesOrCell', moveByLinesOrCell)
+        //
+        // (CodeMirror as any).Vim.mapCommand(
+        //     "<Plug>(vim-binding-k)", "motion", "moveByLinesOrCell",
+        //     { forward: false, linewise: true },
+        //     { context: "normal" }
+        // )
         
         commands.addCommand('run-select-next-edit', {
             label: 'Run Cell and Edit Next Cell',
